a4017db1f90564a89c549517284e0b1be5dbdc63
clicks88 <115968573+clicks88@users.noreply.github.com>
2022-12-23 01:11:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ document.querySelectorAll("button")[i].addEventListener("click", hello);
 function hello(){
     switch(this.innerHTML){
     case "w":
-        var audio= new Audio('sounds/crash.mp3');
+        var audio= new Audio('crash.mp3');
         audio.play();
      case "a":
-        var audio= new Audio('sounds/kick-bass.mp3');
+        var audio= new Audio('kick-bass.mp3');
         audio.play();
      case "s":
-        var audio= new Audio('sounds/snare.mp3');
+        var audio= new Audio('snare.mp3');
         audio.play();
      case "d":
-        var audio= new Audio('sounds/tom-1.mp3');
+        var audio= new Audio('tom-1.mp3');
         audio.play();
   case "j":
-        var audio= new Audio('sounds/tom-2.mp3');
+        var audio= new Audio('tom-2.mp3');
         audio.play();
      case "k":
-        var audio= new Audio('sounds/tom-3.mp3');
+        var audio= new Audio('tom-3.mp3');
         audio.play();
      case "l":
-        var audio= new Audio('sounds/tom-4.mp3');
+        var audio= new Audio('tom-4.mp3');
         audio.play();
 
         
@@ -41,25 +41,25 @@ function hello(){
 
         switch(e.key){
             case "w":
-                var audio= new Audio('sounds/crash.mp3');
+                var audio= new Audio('crash.mp3');
                 audio.play();
              case "a":
-                var audio= new Audio('sounds/kick-bass.mp3');
+                var audio= new Audio('kick-bass.mp3');
                 audio.play();
              case "s":
-                var audio= new Audio('sounds/snare.mp3');
+                var audio= new Audio('snare.mp3');
                 audio.play();
              case "d":
-                var audio= new Audio('sounds/tom-1.mp3');
+                var audio= new Audio('tom-1.mp3');
                 audio.play();
           case "j":
-                var audio= new Audio('sounds/tom-2.mp3');
+                var audio= new Audio('tom-2.mp3');
                 audio.play();
              case "k":
-                var audio= new Audio('sounds/tom-3.mp3');
+                var audio= new Audio('tom-3.mp3');
                 audio.play();
              case "l":
-                var audio= new Audio('sounds/tom-4.mp3');
+                var audio= new Audio('tom-4.mp3');
                 audio.play();
                 
             }
